9944bcfaf625793cf33ae096962dbf4cc8e5024d
KlnSdr <monsieurk1209@gmail.com>
2024-11-13 10:03:22 +0100

    initial commit

---
+++ src/nyx/Main.java
@@ -0,0 +1,20 @@
+package nyx;
+
+import dobby.Dobby;
+import dobby.DobbyEntryPoint;
+
+public class Main implements DobbyEntryPoint {
+    public static void main(String[] args) {
+        Dobby.startApplication(Main.class);
+    }
+
+    @Override
+    public void preStart() {
+
+    }
+
+    @Override
+    public void postStart() {
+
+    }
+}
